f171986c8149c6a7b123dc4055c52a508df4eecd
Petr <p_bolotov@mail.ru>
2013-01-28 14:39:26 +0400

    Solution for task 4(screenshot maker) added : files screenshot_maker.init and screenshot_maker.sh and also a config file screenshot_maker.config

---
+++ screenshot_maker.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+config_file="./screenshot_maker.config"
+username_string=$LOGNAME"_INTERVAL="
+dump_interval=-1
+default_dump_interval=-1
+
+while read line
+do
+	if [[ $line =~ "STORAGE=" ]]
+	then
+		storage_dir=$(echo $line | sed "s|^STORAGE=||" | sed "s|\"||g")
+	elif [[ $line =~ "DUMP_INTERVAL=" ]]
+	then
+		default_dump_interval=$(echo $line | sed "s|^DUMP_INTERVAL=||")
+	elif [[ $line =~ "$username_string" ]]
+	then
+		dump_interval=$(echo $line | sed "s|$username_string||")
+	fi
+done < $config_file
+
+if ((dump_interval == -1))
+then
+	if ((default_dump_interval != -1))
+	then
+		dump_interval=$default_dump_interval
+	else
+		echo "Default dump interval is not specified." > log.txt
+		exit 1
+	fi
+fi
+
+while true
+do
+	xwd -root | convert - "$storage_dir/screenshot_$LOGNAME_$(date +"%Y-%m-%d_%H-%M-%S").png"
+	sleep 5s
+done
